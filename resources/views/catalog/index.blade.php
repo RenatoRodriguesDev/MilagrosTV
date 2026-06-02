70b7e612fd5e8f5bc33af2564071585edb6e1d1b
rrodrigues <renato-silva_16@hotmail.com>
2026-06-02 15:07:22 -0500

    filter fix

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -88,9 +88,8 @@
                 <option value="added"  @selected($sort==='added')  class="bg-gray-900">{{ __('catalog.sort_added') ?? 'Recentes' }}</option>
             </select>
             <input type="hidden" name="order" value="{{ $order }}">
-            <input type="hidden" name="type"  value="{{ $type }}">
             @if($search || $genre)
-            <a href="{{ route('catalog.index', ['type' => $type]) }}" class="text-gray-500 hover:text-white text-sm transition">✕ {{ __('catalog.clear') }}</a>
+            <a href="{{ route('catalog.index') }}" class="text-gray-500 hover:text-white text-sm transition">✕ {{ __('catalog.clear') }}</a>
             @endif
         </form>
         <script>
